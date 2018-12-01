5b0ea9f53d75c9de75612453ee562777c2abac18
lenonguyen@gmail.com <lenonguyen@gmail.com>
2018-12-01 15:08:57 +0200

    Create a charge object

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,8 +2,10 @@ import React, {Component} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
+
 import Header from './Header';
 import Landing from './Landing';
+
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 
