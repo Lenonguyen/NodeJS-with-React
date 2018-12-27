fc5d9d678aa42880d9003f2e3411e404ce589cd5
lenonguyen@gmail.com <lenonguyen@gmail.com>
2018-12-27 23:18:56 +0200

    Display Invalid Emails

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
-
+import validateEmails from '../../utils/validateEmails';
 const FIELDS = [
           { label : 'Survey Title', name : 'title' },
           { label : 'Subject Line', name : 'subject' },
@@ -41,11 +41,14 @@ class SurveyForm extends Component {
 function validate(values) {
      const errors = {};
 
+     errors.emails = validateEmails(values.emails || '');
+
      _.each(FIELDS, ({ name }) => {
           if (!values[name]) {
                errors[name] = 'You must provide a value';
           }
      });
+
 
      return errors;
 }
